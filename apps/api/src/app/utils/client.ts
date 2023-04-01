94f53099605b22f46f44812b11cd371b14b1be8d
Jake Klassen <jklassendev@gmail.com>
2023-03-31 21:48:25 -0400

    remove test db url

--- apps/api/src/app/utils/client.ts
+++ apps/api/src/app/utils/client.ts
@@ -1,13 +1,5 @@
 import { PrismaClient } from '@prisma/client'
 
-console.log(process.env.TEST_DATABASE_URL)
-
-const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.TEST_DATABASE_URL
-    }
-  }
-})
+const prisma = new PrismaClient()
 
 export default prisma
